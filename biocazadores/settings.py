95cf9afe81f7a2aa419b064c790d90317212505e
unknown <foziel73@gmail.com>
2025-11-24 02:32:05 -0600

    si

--- biocazadores/settings.py
+++ biocazadores/settings.py
@@ -61,11 +61,13 @@ WSGI_APPLICATION = 'biocazadores.wsgi.application'
 ASGI_APPLICATION = 'biocazadores.asgi.application'
 
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+    "default": dj_database_url.config(
+           default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 # Validación de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
